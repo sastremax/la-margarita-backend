2aecb08ab4801b91e36a3505131996b306d9face
Maximiliano Sastre <sastrebocalonmaximiliano@gmail.com>
2025-06-18 17:47:26 -0300

    APP
    - En src\app.js:
      Se integró el router principal importado desde routes\index.js bajo el prefijo /api.-

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import cors from 'cors'
+import router from './routes/index.js'
 
 const app = express()
 
@@ -7,6 +8,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+app.use('/api', router)
 app.get('/ping', (req, res) => {
     res.json({ message: 'pong' })
 })
