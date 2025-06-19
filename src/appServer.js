ee6685c97b328f6c4bf83103bedbae97dab827c9
Maximiliano Sastre <sastrebocalonmaximiliano@gmail.com>
2025-06-19 11:11:18 -0300

    FEAT(CONFIG)-APPSERVER
    - En src\APPSERVER.JS: Se reemplazó la conexión directa con mongoose por el uso de connectToDB desde config\db.js. Se inicializó Passport con app.use(passport.initialize()). Se dejó preparado el entorno para futuras integraciones como Swagger.

--- src/appServer.js
+++ src/appServer.js
@@ -1,22 +1,23 @@
-import mongoose from 'mongoose';
-import config from './config/index.js';
-import logger from './utils/logger.js';
-import app from './app.js';
+import config from './config/index.js'
+import logger from './utils/logger.js'
+import app from './app.js'
+import { connectToDB } from './config/db.js'
+import passport from './config/passport.config.js'
 
 const startServer = async () => {
     try {
-        await mongoose.connect(config.mongoUri);
-        logger.info('Connected to MongoDB');
+        await connectToDB()
+
+        app.use(passport.initialize())
 
         app.listen(config.port, () => {
-            logger.info(`Server listening on port ${config.port}`);
-        });
+            logger.info(`Server listening on port ${config.port}`)
+        })
     } catch (err) {
-        logger.error('Failed to start server');
-        logger.error(err);
-        process.exit(1);
+        logger.fatal('Failed to start server')
+        logger.fatal(err)
+        process.exit(1)
     }
-};
+}
 
-startServer();
-
+startServer()
