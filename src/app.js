65fe787a5db978ef9dbc538b34a9c4a3d4f24eb7
Maximiliano Sastre <sastrebocalonmaximiliano@gmail.com>
2025-06-19 09:44:18 -0300

    FEAT(APP)
    - En src\app.js: Se integraron los middlewares globales corsConfig, requestLogger, rateLimit, sanitize, trimBody, notFound y errorHandler.

--- src/app.js
+++ src/app.js
@@ -1,16 +1,26 @@
 import express from 'express'
-import cors from 'cors'
+import corsConfig from './middlewares/corsConfig.middleware.js'
+import requestLogger from './middlewares/requestLogger.middleware.js'
+import rateLimit from './middlewares/rateLimit.middleware.js'
+import sanitize from './middlewares/sanitize.middleware.js'
+import trimBody from './middlewares/trimBody.middleware.js'
+import notFound from './middlewares/notFound.middleware.js'
+import errorHandler from './middlewares/errorHandler.middleware.js'
 import router from './routes/index.js'
 
 const app = express()
 
-app.use(cors())
+app.use(corsConfig)
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(trimBody)
+app.use(sanitize)
+app.use(requestLogger)
+app.use(rateLimit)
 
 app.use('/api', router)
-app.get('/ping', (req, res) => {
-    res.json({ message: 'pong' })
-})
+
+app.use(notFound)
+app.use(errorHandler)
 
 export default app
