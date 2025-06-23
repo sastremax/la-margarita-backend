bbb13b0757226af86ee695cd8a4ba2c38b6890bf
Maximiliano Sastre <sastrebocalonmaximiliano@gmail.com>
2025-06-23 14:43:22 -0300

    LODGING.SERVICE
    - En src/services/lodging.service.js: Se corrigió el uso de LodgingDAO instanciando la clase correctamente antes de llamar a sus métodos.

--- src/services/lodging.service.js
+++ src/services/lodging.service.js
@@ -1,24 +1,25 @@
 import LodgingDAO from '../dao/lodging.dao.js'
+const lodgingDAO = new LodgingDAO()
 
 class LodgingService {
     static async getAllLodgings() {
-        return await LodgingDAO.getAllLodgings()
+        return await lodgingDAO.getAllLodgings()
     }
 
     static async getLodgingById(id) {
-        return await LodgingDAO.getLodgingById(id)
+        return await lodgingDAO.getLodgingById(id)
     }
 
     static async createLodging(lodgingData) {
-        return await LodgingDAO.createLodging(lodgingData)
+        return await lodgingDAO.createLodging(lodgingData)
     }
 
     static async updateLodging(id, updateData) {
-        return await LodgingDAO.updateLodging(id, updateData)
+        return await lodgingDAO.updateLodging(id, updateData)
     }
 
     static async deleteLodging(id) {
-        return await LodgingDAO.deleteLodging(id)
+        return await lodgingDAO.deleteLodging(id)
     }
 }
 
