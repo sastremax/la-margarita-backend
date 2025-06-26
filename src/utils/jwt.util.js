6e2d3e5effade7dc65e61c2f36ff91331fd827cf
Maximiliano Sastre <sastrebocalonmaximiliano@gmail.com>
2025-06-26 13:55:56 -0300

    FEAT(AUTH)-JWT.UTIL-JS
    - En SRC\UTILS\JWT.UTIL.JS: Se creó el archivo con funciones para generar y verificar access y refresh tokens, con expiración corta y uso seguro para autenticación basada en cookies httpOnly.

--- src/utils/jwt.util.js
+++ src/utils/jwt.util.js
@@ -2,19 +2,37 @@ import jwt from 'jsonwebtoken'
 import config from '../config/index.js'
 import ApiError from './apiError.js'
 
-function createToken(payload, expiresIn = config.jwtExpiresIn) {
-    return jwt.sign(payload, config.jwtSecret, { expiresIn })
+const createAccessToken = (user) => {
+    const payload = {
+        id: user._id,
+        role: user.role
+    }
+
+    return jwt.sign(payload, config.jwtSecret, {
+        expiresIn: '15m'
+    })
 }
 
-function verifyToken(token) {
+const createRefreshToken = (user) => {
+    const payload = {
+        id: user._id
+    }
+
+    return jwt.sign(payload, config.jwtRefreshSecret, {
+        expiresIn: '7d'
+    })
+}
+
+const verifyToken = (token, secret = config.jwtSecret) => {
     try {
-        return jwt.verify(token, config.jwtSecret)
+        return jwt.verify(token, secret)
     } catch {
         throw new ApiError(401, 'Invalid or expired token')
     }
 }
 
 export default {
-    createToken,
+    createAccessToken,
+    createRefreshToken,
     verifyToken
 }
