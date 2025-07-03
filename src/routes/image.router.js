5713d75cb28a320e6b78574aa6a3cfc6634f54bb
Maximiliano Sastre <sastrebocalonmaximiliano@gmail.com>
2025-07-03 15:34:47 -0300

    REFACTOR(IMAGE)-ROUTER
    - En src\routes\image.router.js: Se protegieron todas las rutas con authPolicy(['admin']). Se aplicó validateDTO con imageDTO.imageSchema en la ruta POST. Se ordenaron correctamente las rutas para evitar conflictos de parámetros.

--- src/routes/image.router.js
+++ src/routes/image.router.js
@@ -6,7 +6,19 @@ import imageDTO from '../dto/image.dto.js'
 
 const router = express.Router()
 
-router.post('/', authPolicy(['admin']), validateDTO(imageDTO.imageSchema), imageController.uploadImage)
-router.delete('/:iid', authPolicy(['admin']), imageController.deleteImage)
+router.post(
+    '/',
+    authPolicy(['admin']),
+    validateDTO(imageDTO.imageSchema),
+    imageController.uploadImage
+)
+
+router.get('/', authPolicy(['admin']), imageController.getAllImages)
+
+router.get('/:id', authPolicy(['admin']), imageController.getImageById)
+
+router.get('/lodging/:lodgingId', authPolicy(['admin']), imageController.getImagesByLodgingId)
+
+router.delete('/:id', authPolicy(['admin']), imageController.deleteImage)
 
 export default router
