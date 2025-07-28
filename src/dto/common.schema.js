c03fd856bcfb98ae1b9de70ed13a4fde6dfd93b5
Maximiliano Sastre <sastrebocalonmaximiliano@gmail.com>
2025-07-28 20:23:02 -0300

    REFACTOR(DTO)-REMOVE-DEFAULT-EXPORT-COMMON-SCHEMA
    - En src/dto/common.schema.js: Se eliminó la exportación por defecto. Se reemplazó por export const passwordSchema.

--- src/dto/common.schema.js
+++ src/dto/common.schema.js
@@ -1,9 +1,7 @@
 import z from 'zod'
 
-const passwordSchema = z.string()
+export const passwordSchema = z.string()
     .min(8, 'Password must be at least 8 characters long')
     .regex(/^(?=.*[a-zA-Z])(?=.*\d)(?=.*[\W_]).+$/, {
         message: 'Password must contain at least one letter, one number, and one special character'
     })
-
-export default passwordSchema
