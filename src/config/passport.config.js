3fea2f1d4fa2f17e82fdb7f246878847045cb627
Maximiliano Sastre <sastrebocalonmaximiliano@gmail.com>
2025-07-29 15:51:59 -0300

    passport
    - En src\config\passport.config.js: Se modifican las importaciones.-

--- src/config/passport.config.js
+++ src/config/passport.config.js
@@ -1,6 +1,7 @@
-import { config } from './index.js'
-import { Strategy as JwtStrategy, ExtractJwt } from 'passport-jwt'
 import passport from 'passport'
+import { Strategy as JwtStrategy, ExtractJwt } from 'passport-jwt'
+
+import { config } from './index.js'
 import User from '../models/user.model.js'
 
 const opts = {
