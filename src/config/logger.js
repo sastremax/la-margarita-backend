01372e4f203b64429f83a032c9b5afbb3456801b
Maximiliano Sastre <sastrebocalonmaximiliano@gmail.com>
2025-07-30 13:41:38 -0300

    REFACTOR(CONFIG)-USE-CENTRALIZED-LOGLEVEL
    - En src/config/logger.js: Se reemplazó el acceso directo a process.env.LOG_LEVEL por config.logLevel, centralizando la gestión de variables de entorno en config/index.js.

--- src/config/logger.js
+++ src/config/logger.js
@@ -1,11 +1,14 @@
+
 import { addColors } from 'winston/lib/winston/config/index.js'
 import { customLevels } from './customLevels.js'
 import { devLogger } from './devLogger.js'
+import { config } from './index.js'
 import { prodLogger } from './prodLogger.js'
 import { testLogger } from './testLogger.js'
 
+
 const nodeEnv = process.env.NODE_ENV || 'dev'
-const logLevel = process.env.LOG_LEVEL || 'info'
+const logLevel = config.logLevel || 'info'
 
 addColors(customLevels.colors)
 
