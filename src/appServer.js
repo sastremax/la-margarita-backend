9dc410f7f026fee2d0c7f21c911c988316173eca
Maximiliano Sastre <sastrebocalonmaximiliano@gmail.com>
2025-08-19 11:16:48 -0300

    FEAT(SCRIPTS)-DEV-TEST
    - En package.json: se modificó el script dev:test para forzar la variable FORCE_LISTEN y levantar el servidor en el puerto 4001 con el entorno de test.
    - En src/appServer.js: se ajustó la lógica de startServer para que respete FORCE_LISTEN=true en modo test y muestre correctamente el log de inicio en puerto 4001.

--- src/appServer.js
+++ src/appServer.js
@@ -1,23 +1,21 @@
+import passport from 'passport'
+import { connectToDB } from './config/db.js'
 import { config } from './config/index.js'
 import { logger } from './config/logger.js'
-import { app } from './appExpress.js'
-import { connectToDB } from './config/db.js'
-import passport from 'passport'
 import './config/passport.config.js'
+import { app } from './appExpress.js'
 
 const PORT = config.port
 
 export const startServer = async () => {
-    if (process.env.NODE_ENV === 'test') {
+    const shouldListen = process.env.NODE_ENV !== 'test' || process.env.FORCE_LISTEN === 'true'
+    if (!shouldListen) {
         logger.info('Skipping startServer() in test mode')
         return
     }
-
     try {
         await connectToDB()
-
         app.use(passport.initialize())
-
         app.listen(PORT, () => {
             logger.info(`Server listening on port ${PORT}`)
         })
@@ -27,3 +25,5 @@ export const startServer = async () => {
         process.exit(1)
     }
 }
+
+startServer()
